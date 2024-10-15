691362221471b5185d8e9e8df08f3b7232d81c58
RakeshRMaasti <42163375+RakeshRMaasti@users.noreply.github.com>
2024-10-15 19:47:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
 }
 
 # Azure provider setup
