d6693d05dee6e2a58858a5c2e6b4d5ca4e8a5a22
RakeshRMaasti <42163375+RakeshRMaasti@users.noreply.github.com>
2024-10-15 18:20:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,95 @@
+# main.tf
 provider "azurerm" {
-  subscription_id = "90de0c98-2d25-4094-a834-32b29fdf8003"
   features {}
 }
 
-# Resource Group
+# Azure Resource Group
 resource "azurerm_resource_group" "aks_rg" {
   name     = "aks-resource-group"
   location = "East US"
 }
 
-# Azure Container Registry (ACR)
-resource "azurerm_container_registry" "acr" {
-  name                = "acrsqaud6"
-  resource_group_name = azurerm_resource_group.aks_rg.name
+# Azure Virtual Network
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "aks-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.aks_rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
+  resource_group_name = azurerm_resource_group.aks_rg.name
 }
 
-# Azure Kubernetes Service (AKS)
+# Subnet for AKS Cluster
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Azure Kubernetes Service Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "myaks"
+  dns_prefix          = "aks"
 
   default_node_pool {
     name       = "default"
     node_count = 2
     vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
 
   identity {
     type = "SystemAssigned"
   }
 
+  # Role-based Access Control (RBAC)
+  role_based_access_control {
+    enabled = true
+  }
+
   network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+    network_plugin = "azure"
+    dns_service_ip = "10.0.2.10"
+    service_cidr   = "10.0.2.0/24"
+    docker_bridge_cidr = "172.17.0.1/16"
   }
 }
 
-# Additional Node Pool
-resource "azurerm_kubernetes_cluster_node_pool" "aks_nodes" {
-  name                  = "systempool"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 2
+# Azure Container Registry (ACR) 
+resource "azurerm_container_registry" "acr" {
+  name                = "bayeracr"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
-# Outputs
-output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+# Attach ACR to AKS
+resource "azurerm_role_assignment" "aks_acr_assignment" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
 }
 
-output "container_registry" {
-  value = azurerm_container_registry.acr.login_server
+# Log Analytics Workspace for monitoring
+resource "azurerm_log_analytics_workspace" "aks_log" {
+  name                = "aks-log-analytics"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  sku                 = "PerGB2018"
 }
 
+# AKS Monitoring Solution
+resource "azurerm_kubernetes_cluster" "aks" {
+  monitoring {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.aks_log.id
+  }
+}
+
+# Output the Kubernetes configuration for kubectl
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
 
