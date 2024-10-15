81e01061aa4f22a824b57dfe09b9519cb8b79589
RakeshRMaasti <42163375+RakeshRMaasti@users.noreply.github.com>
2024-10-15 15:08:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "aks_rg" {
   name     = "aks-resource-group"
   location = "East US"
 }
 
+# Azure Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
   name                = "myACR"
   resource_group_name = azurerm_resource_group.aks_rg.name
@@ -15,11 +17,12 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
+# Azure Kubernetes Service (AKS)
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "myaks"
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
     name       = "default"
@@ -33,22 +36,36 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin    = "azure"
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard" # Corrected to lowercase
     network_policy    = "calico"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].node_count
+    ]
   }
 }
 
-resource "azurerm_kubernetes_cluster_node_pool" "aks_nodes" {
-  name                  = "systempool"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 2
-}
-
+# Output the AKS Cluster details
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "container_registry" {
+output "kubernetes_cluster_url" {
+  value = azurerm_kubernetes_cluster.aks.kube_config[0].host
+}
+
+# Output the ACR login details
+output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value = azurerm_container_registry.acr.admin_password
+}
+
